48df75206ecded2690507dbfc60fe56b63a0f82e
Ivan Herger <ivan.herger@gmail.com>
2019-10-25 15:51:34 +0200

    Add Shares contract

---
+++ src/contracts/Shares.test.ts
@@ -0,0 +1,27 @@
+import { Eth } from 'web3-eth';
+import { HttpProvider } from 'web3-providers';
+import { Environment } from '../Environment';
+import deployment from '../deployments/mainnet';
+import { Shares } from './Shares';
+
+describe('Shares', () => {
+  let environment: Environment;
+  let shares: Shares;
+
+  beforeAll(() => {
+    // TODO: This should be replaced with a local ganache test environment using proper test fixtures.
+    const client = new Eth(new HttpProvider('https://mainnet.melonport.com'));
+    environment = new Environment(client, deployment);
+    shares = new Shares(environment, '0x70958d69bd1a550b5f49d4d64d1c8bfa358558cd');
+  });
+
+  it('should return name for shares', async () => {
+    const result = await shares.getName();
+    expect(result.length).toBeGreaterThanOrEqual(0);
+  });
+
+  it('should return symbol for shares', async () => {
+    const result = await shares.getSymbol();
+    expect(result).toBe('MLNF');
+  });
+});
