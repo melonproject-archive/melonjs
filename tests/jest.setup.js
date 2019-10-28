c13a0affe097d3d9c933d0d11b6c21b8864984a5
Sebastian Siemssen <fubhy@fubhy.com>
2019-10-28 17:15:44 +0100

    Increase test timeout.

--- tests/jest.setup.js
+++ tests/jest.setup.js
@@ -1,5 +1,8 @@
 const ganache = require('ganache-core');
 
+// Set the async timeout to 10 seconds (default 5 seconds).
+jest.setTimeout(10 * 1000);
+
 module.exports = () => {
   global.__GANACHE__ = ganache.server();
   global.__GANACHE__.listen(process.env.GANACHE_PORT || 8545);
