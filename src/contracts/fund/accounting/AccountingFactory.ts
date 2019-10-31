1de62449ba0dbc094ad274c24f3ab94ab1a28ca1
Ivan Herger <ivan.herger@gmail.com>
2019-10-31 09:29:46 +0100

    AccountingFactory

---
+++ src/contracts/fund/accounting/AccountingFactory.ts
@@ -0,0 +1,33 @@
+import { Contract } from '../../../Contract';
+import { Environment } from '../../../Environment';
+import { Address } from '../../../Address';
+import { applyMixins } from '../../../utils/applyMixins';
+import { AccountingFactoryAbi } from '../../../abis/AccountingFactory.abi';
+import { Factory } from '../../factory/Factory';
+import { AccountingDeployArguments } from './Accounting';
+
+export class AccountingFactory extends Contract {
+  public static readonly abi = AccountingFactoryAbi;
+
+  public static deploy(environment: Environment, bytecode: string, from: Address) {
+    return super.createDeployment<AccountingFactory>(environment, bytecode, from);
+  }
+
+  /**
+   * Creates an Accounting instance.
+   *
+   * @param from The sender address.
+   * @param args The accounting deploy arguments as [[AccountingDeployArguments]].
+   */
+  public createInstance(from: Address, args: AccountingDeployArguments) {
+    return this.createTransaction('createInstance', from, [
+      args.hub,
+      args.denominationAsset,
+      args.nativeAsset,
+      args.defaultAssets,
+    ]);
+  }
+}
+
+export interface AccountingFactory extends Factory {}
+applyMixins(AccountingFactory, [Factory]);
