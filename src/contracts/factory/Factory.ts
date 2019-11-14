40ed90122a7707c579b2af5159960bb96d0a69a3
Ivan Herger <ivan.herger@gmail.com>
2019-11-14 11:41:10 +0100

    Remove unnecessary async

--- src/contracts/factory/Factory.ts
+++ src/contracts/factory/Factory.ts
@@ -16,7 +16,7 @@ export class Factory extends Contract {
    * @param address The address of the contract to check
    * @param block The block number to execute the call on.
    */
-  public async isInstance(address: Address, block?: number) {
+  public isInstance(address: Address, block?: number) {
     return this.makeCall<boolean>('isInstance', [address], block);
   }
 }
