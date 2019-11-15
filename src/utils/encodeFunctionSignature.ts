06de971ea2e408aeacbc7fe294cb1ce30017aa7f
Ivan Herger <ivan.herger@gmail.com>
2019-11-15 11:56:09 +0100

    Add encodeFunctionSignature util function

---
+++ src/utils/encodeFunctionSignature.ts
@@ -0,0 +1,9 @@
+import { AbiItem } from 'web3-utils';
+import { AbiCoder } from 'web3-eth-abi';
+
+export function encodeFunctionSignature(abi: AbiItem[], name: string): string {
+  const abiItem = abi.find(item => item.name === name);
+  const abiCoder = new AbiCoder();
+  const signature = abiCoder.encodeFunctionSignature(abiItem);
+  return signature;
+}
