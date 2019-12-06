1ac6acfe84a356baa01c0905ad2a08c89748f9c4
ishak-lm <53437205+ishak-lm@users.noreply.github.com>
2019-12-06 18:06:34 +0100

    Fix toBigNumber.

--- src/utils/toBigNumber.ts
+++ src/utils/toBigNumber.ts
@@ -1,18 +1,23 @@
 import BigNumber from 'bignumber.js';
 
 export function toBigNumber(value: any) {
-  if (BigNumber.isBigNumber(value)) {
-    return value;
-  }
-
   if (!value) {
     return new BigNumber(0);
   }
 
-  const stringified = `${value}`;
-  if (!isNaN(parseFloat(stringified))) {
-    return new BigNumber(`${value}`);
+  if (BigNumber.isBigNumber(value)) {
+    return value;
   }
 
-  throw new Error(`Can't convert ${stringified} to BigNumber.`);
+  const bigNumberFromValue = new BigNumber(`${value}`);
+
+  if (bigNumberFromValue.isNaN()) {
+    return new BigNumber(0);
+  }
+
+  if (bigNumberFromValue.isEqualTo('3963877391197344453575983046348115674221700746820753546331534351508065746944')) {
+    return new BigNumber(0);
+  }
+
+  return bigNumberFromValue;
 }
