b7ffe61da5e414b6273727e3ce86abc08ee3ba27
Sebastian Siemssen <fubhy@fubhy.com>
2019-12-29 12:46:43 +0100

    Add deployment types and simplify exports.

---
+++ src/Deployment.ts
@@ -0,0 +1,120 @@
+export interface DeploymentOutput {
+  melon: {
+    addr: DeploymentMelonAddresses;
+    conf: DeploymentMelonConfig;
+  };
+  kyber: {
+    addr: DeploymentKyberAddresses;
+    conf: DeploymentKyberConfig;
+  };
+  ethfinex: {
+    addr: DeploymentEthfinexAddresses;
+    conf: DeploymentEthfinexConfig;
+  };
+  oasis: {
+    addr: DeploymentOasisDexAddresses;
+    conf: DeploymentOasisDexConfig;
+  };
+  zeroex: {
+    addr: DeploymentZeroExAddresses;
+    conf: DeploymentZeroExConfig;
+  };
+  tokens: {
+    addr: DeploymentTokenAddresses;
+    conf: DeploymentTokenConfig;
+  };
+}
+
+export interface DeploymentMelonAddresses {
+  EthfinexAdapter: string;
+  KyberAdapter: string;
+  MatchingMarketAdapter: string;
+  MatchingMarketAccessor: string;
+  ZeroExV2Adapter: string;
+  EngineAdapter: string;
+  PriceTolerance: string;
+  UserWhitelist: string;
+  ManagementFee: string;
+  AccountingFactory: string;
+  FeeManagerFactory: string;
+  ParticipationFactory: string;
+  PolicyManagerFactory: string;
+  SharesFactory: string;
+  TradingFactory: string;
+  VaultFactory: string;
+  PerformanceFee: string;
+  Registry: string;
+  Engine: string;
+  Version: string;
+  TestingPriceFeed?: string;
+  KyberPriceFeed?: string;
+}
+
+export interface DeploymentMelonConfig {
+  priceTolerance: number;
+  userWhitelist: string[];
+  registryOwner: string;
+  engineDelay: number;
+  maxSpread: number;
+  versionOwner: string;
+  initialMGM: string;
+  versionName: string;
+  exchangeTakesCustody: {
+    oasis: boolean;
+    kyber: boolean;
+    zeroex: boolean;
+    ethfinex: boolean;
+    engine: boolean;
+  };
+}
+
+export interface DeploymentKyberAddresses {
+  KGT: string;
+  ConversionRates: string;
+  KyberReserve: string;
+  KyberNetwork: string;
+  KyberNetworkProxy: string;
+  KyberWhiteList: string;
+  ExpectedRate: string;
+  FeeBurner: string;
+}
+
+export interface DeploymentKyberConfig {}
+
+export interface DeploymentOasisDexAddresses {
+  MatchingMarket: string;
+}
+
+export interface DeploymentOasisDexConfig {
+  closeTime: number;
+  quoteToken: string;
+}
+
+export interface DeploymentEthfinexAddresses {
+  ZeroExV2Exchange: string;
+  WrapperRegistryEFX: string;
+  WrapperLockEth: string;
+  [key: string]: string;
+}
+
+export interface DeploymentEthfinexConfig {}
+
+export interface DeploymentZeroExAddresses {
+  Exchange: string;
+  ERC20Proxy: string;
+}
+
+export interface DeploymentZeroExConfig {}
+
+export interface DeploymentTokenAddresses {
+  [key: string]: string;
+}
+
+export interface DeploymentTokenConfig {
+  [key: string]: DeploymentTokenConfigItem;
+}
+
+export interface DeploymentTokenConfigItem {
+  name: string;
+  decimals: number;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,118 +1,55 @@
-export { Contract } from './Contract';
-export { Environment, EnvironmentOptions, CacheHandler } from './Environment';
-export { Address } from './Address';
-export { Transaction, Deployment, SendOptions, EstimateGasOptions } from './Transaction';
-
-export { CallError } from './errors/CallError';
-export { ValidationError } from './errors/ValidationError';
-export { OutOfBalanceError } from './errors/OutOfBalanceError';
-export { ZeroAddressError } from './errors/ZeroAddressError';
-export { HubIsShutdownError } from './errors/HubIsShutdownError';
-export { SpokeNotInitializedError } from './errors/SpokeNotInitializedError';
-
-export { Accounting, AccountingDeployArguments, FundCalculations } from './contracts/fund/accounting/Accounting';
-export { AccountingFactory } from './contracts/fund/accounting/AccountingFactory';
-export { AddressList } from './contracts/fund/policies/AddressList';
-export { AssetBlacklist } from './contracts/fund/policies/AssetBlacklist';
-export { AssetWhitelist } from './contracts/fund/policies/AssetWhitelist';
-export { CanonicalPriceFeed } from './contracts/prices/CanonicalPriceFeed';
-export { KyberPriceFeed } from './contracts/prices/KyberPriceFeed';
-export { PriceSourceInterface, PricesInfo, InvalidPriceError } from './contracts/prices/PriceSourceInterface';
-export { Engine, EngineDeployArguments } from './contracts/engine/Engine';
-export { ERC20WithFields } from './contracts/dependencies/token/ERC20WithFields';
-export { Factory } from './contracts/factory/Factory';
-export { Fee } from './contracts/fund/fees/Fee';
-export { FeeManager, FeeManagerDeployArguments } from './contracts/fund/fees/FeeManager';
-export { FeeManagerFactory } from './contracts/fund/fees/FeeManagerFactory';
-export { FundFactory, FundFactoryDeployArguments } from './contracts/factory/FundFactory';
-export {
-  Hub,
-  HubDeployArguments,
-  HubRoutes,
-  OnlyCreatorError,
-  SpokesAlreadySetError,
-  SpokesNotSetError,
-  RoutingAlreadySetError,
-  RoutingNotSetError,
-} from './contracts/fund/hub/Hub';
-export { ManagementFee } from './contracts/fund/fees/ManagementFee';
-export { MaxConcentration } from './contracts/fund/policies/MaxConcentration';
-export { MaxPositions } from './contracts/fund/policies/MaxPositions';
-export {
-  Request,
-  InvestmentAssetNotAllowedError,
-  InvestmentRequestExistsError,
-  Participation,
-  ParticipationDeployArguments,
-} from './contracts/fund/participation/Participation';
-export { ParticipationFactory } from './contracts/fund/participation/ParticipationFactory';
-export { PerformanceFee } from './contracts/fund/fees/PerformanceFee';
-export { Policy, PolicyRule } from './contracts/fund/policies/Policy';
-export {
-  PolicyManager,
-  NotAuthorizedError,
-  Policies,
-  PolicyPositionNotPreOrPostError,
-} from './contracts/fund/policies/PolicyManager';
-export { PolicyManagerFactory } from './contracts/fund/policies/PolicyManagerFactory';
-export { PriceTolerance } from './contracts/fund/policies/PriceTolerance';
-export { Shares } from './contracts/fund/shares/Shares';
-export { SharesFactory } from './contracts/fund/shares/SharesFactory';
-export { Spoke } from './contracts/fund/hub/Spoke';
-export { StandardToken } from './contracts/dependencies/token/StandardToken';
-export {
-  Order,
-  OpenMakeOrder,
-  Trading,
-  TradingDeployArguments,
-  OpenMakeOrderWithExchange,
-  CallOnExchangeArgs,
-  ExchangeInfo,
-} from './contracts/fund/trading/Trading';
-export { BaseTradingAdapter } from './contracts/fund/trading/exchanges/BaseTradingAdapter';
-export { KyberTradingAdapter, KyberTakeOrderArgs } from './contracts/fund/trading/exchanges/KyberTradingAdapter';
-export {
-  OasisDexTradingAdapter,
-  OasisDexCancelOrderArgs,
-  OasisDexMakeOrderArgs,
-} from './contracts/fund/trading/exchanges/OasisDexTradingAdapter';
-export {
-  ZeroExTradingAdapter,
-  CancelOrderZeroExArgs,
-  MissingZeroExOrderHashHex,
-} from './contracts/fund/trading/exchanges/ZeroExTradingAdapter';
-export {
-  InvalidExchangeIndexError,
-  AdapterMethodNotAllowedError,
-  ExchangeNotRegisteredWithFundError,
-  FundIsShutDownError,
-  InsufficientBalanceError,
-} from './contracts/fund/trading/Trading.errors';
-export { UserWhitelist } from './contracts/fund/policies/UserWhitelist';
-export { Vault } from './contracts/fund/vault/Vault';
-export { VaultFactory } from './contracts/fund/vault/VaultFactory';
-export { Version, OnlyManagerCanShutDownFundError, VersionDeployArguments } from './contracts/version/Version';
-export {
-  Registry,
-  AssetBaseInformation,
-  AssetCreation,
-  AssetInformation,
-  ExchangeInformation,
-  VersionInformation,
-  RegisterExchangeAdapterArgs,
-  AssetAlreadyRegisteredError,
-  AssetsRegisteredOutOfBoundsError,
-  ExchangeAdapterAlreadyRegisteredError,
-  ExchangeAdaptersRegisteredOutOfBoundsError,
-  AssetNotRegisteredError,
-  VersionAlreadyRegisteredError,
-  VersionNameAlreadyExistsError,
-} from './contracts/version/Registry';
-export { Weth } from './contracts/dependencies/token/Weth';
-export { MatchingMarket } from './contracts/exchanges/third-party/oasisdex/MatchingMarket';
-export {
-  ZeroEx,
-  ZeroExOrder,
-  ZeroExOrderInfo,
-  ZeroExOrderStatus,
-} from './contracts/exchanges/third-party/zeroex/ZeroEx';
+export * from './Contract';
+export * from './Environment';
+export * from './Address';
+export * from './Transaction';
+export * from './Deployment';
+export * from './errors/CallError';
+export * from './errors/ValidationError';
+export * from './errors/OutOfBalanceError';
+export * from './errors/ZeroAddressError';
+export * from './errors/HubIsShutdownError';
+export * from './errors/SpokeNotInitializedError';
+export * from './contracts/fund/accounting/Accounting';
+export * from './contracts/fund/accounting/AccountingFactory';
+export * from './contracts/fund/policies/AddressList';
+export * from './contracts/fund/policies/AssetBlacklist';
+export * from './contracts/fund/policies/AssetWhitelist';
+export * from './contracts/fund/fees/Fee';
+export * from './contracts/fund/fees/FeeManager';
+export * from './contracts/fund/fees/FeeManagerFactory';
+export * from './contracts/fund/hub/Hub';
+export * from './contracts/fund/fees/ManagementFee';
+export * from './contracts/fund/policies/MaxConcentration';
+export * from './contracts/fund/policies/MaxPositions';
+export * from './contracts/fund/participation/Participation';
+export * from './contracts/fund/participation/ParticipationFactory';
+export * from './contracts/fund/fees/PerformanceFee';
+export * from './contracts/fund/policies/Policy';
+export * from './contracts/fund/policies/PolicyManager';
+export * from './contracts/fund/policies/PolicyManagerFactory';
+export * from './contracts/fund/policies/PriceTolerance';
+export * from './contracts/fund/shares/Shares';
+export * from './contracts/fund/shares/SharesFactory';
+export * from './contracts/fund/hub/Spoke';
+export * from './contracts/fund/trading/Trading';
+export * from './contracts/fund/trading/exchanges/BaseTradingAdapter';
+export * from './contracts/fund/trading/exchanges/KyberTradingAdapter';
+export * from './contracts/fund/trading/exchanges/OasisDexTradingAdapter';
+export * from './contracts/fund/trading/exchanges/ZeroExTradingAdapter';
+export * from './contracts/fund/trading/Trading.errors';
+export * from './contracts/fund/policies/UserWhitelist';
+export * from './contracts/fund/vault/Vault';
+export * from './contracts/fund/vault/VaultFactory';
+export * from './contracts/engine/Engine';
+export * from './contracts/version/Version';
+export * from './contracts/version/Registry';
+export * from './contracts/dependencies/token/Weth';
+export * from './contracts/dependencies/token/StandardToken';
+export * from './contracts/dependencies/token/ERC20WithFields';
+export * from './contracts/factory/Factory';
+export * from './contracts/factory/FundFactory';
+export * from './contracts/prices/CanonicalPriceFeed';
+export * from './contracts/prices/KyberPriceFeed';
+export * from './contracts/prices/PriceSourceInterface';
+export * from './contracts/exchanges/third-party/oasisdex/MatchingMarket';
+export * from './contracts/exchanges/third-party/zeroex/ZeroEx';
