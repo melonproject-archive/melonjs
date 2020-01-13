168039de7e45bc214bc13887fe1bb8936f2fd17f
Ivan Herger <ivan.herger@gmail.com>
2020-01-13 13:38:37 +0100

    Clean up token transactions

--- src/contracts/dependencies/token/BurnableToken.ts
+++ src/contracts/dependencies/token/BurnableToken.ts
@@ -5,18 +5,9 @@ import { applyMixins } from '../../../utils/applyMixins';
 import { PreminedToken, PreminedTokenDeployArguments } from './PreminedToken';
 import { BigNumber } from 'bignumber.js';
 import { BurnableTokenAbi } from '../../../abis/BurnableToken.abi';
-import { sameAddress } from '../../../utils/sameAddress';
-import { zeroAddress } from '../../../utils/zeroAddress';
 import { OutOfBalanceError } from '../../../errors/OutOfBalanceError';
-import { ValidationError } from '../../../errors/ValidationError';
-
-export class AddressCannotBeZeroError extends ValidationError {
-  public name = 'AddressCannotBeZeroError';
-
-  constructor(public readonly address: Address, message: string = 'Sender cannot be zero address') {
-    super(message);
-  }
-}
+import { isZeroAddress } from '../../../utils/isZeroAddress';
+import { ZeroAddressError } from '../../../errors/ZeroAddressError';
 
 export class BurnableToken extends Contract {
   public static readonly abi = BurnableTokenAbi;
@@ -35,9 +26,7 @@ export class BurnableToken extends Contract {
     const args = [amount.toFixed(0)];
 
     const validate = async () => {
-      if (sameAddress(from, zeroAddress)) {
-        throw new AddressCannotBeZeroError(from);
-      }
+      if (isZeroAddress(from)) throw new ZeroAddressError();
 
       const balance = await this.getBalanceOf(from);
       if (!amount.isLessThanOrEqualTo(balance)) {
