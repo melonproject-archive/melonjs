7c68eecd2064b0b33e43289c18d1151404f47a2c
Ivan Herger <ivan.herger@gmail.com>
2020-01-13 13:23:08 +0100

    Add validations for burning a token

--- src/contracts/dependencies/token/BurnableToken.ts
+++ src/contracts/dependencies/token/BurnableToken.ts
@@ -1,13 +1,25 @@
 import { Environment } from '../../../Environment';
 import { Address } from '../../../Address';
 import { Contract } from '../../../Contract';
-import { PreminedTokenAbi } from '../../../abis/PreminedToken.abi';
 import { applyMixins } from '../../../utils/applyMixins';
 import { PreminedToken, PreminedTokenDeployArguments } from './PreminedToken';
 import { BigNumber } from 'bignumber.js';
+import { BurnableTokenAbi } from '../../../abis/BurnableToken.abi';
+import { sameAddress } from '../../../utils/sameAddress';
+import { zeroAddress } from '../../../utils/zeroAddress';
+import { OutOfBalanceError } from '../../../errors/OutOfBalanceError';
+import { ValidationError } from '../../../errors/ValidationError';
+
+export class AddressCannotBeZeroError extends ValidationError {
+  public name = 'AddressCannotBeZeroError';
+
+  constructor(public readonly address: Address, message: string = 'Sender cannot be zero address') {
+    super(message);
+  }
+}
 
 export class BurnableToken extends Contract {
-  public static readonly abi = PreminedTokenAbi;
+  public static readonly abi = BurnableTokenAbi;
 
   public static deploy(environment: Environment, bytecode: string, from: Address, args: PreminedTokenDeployArguments) {
     return super.createDeployment<BurnableToken>(environment, bytecode, from, [args.symbol, args.decimals, args.name]);
@@ -21,7 +33,19 @@ export class BurnableToken extends Contract {
    */
   public burn(from: Address, amount: BigNumber) {
     const args = [amount.toFixed(0)];
-    return this.createTransaction({ from, method: 'decreaseApproval', args });
+
+    const validate = async () => {
+      if (sameAddress(from, zeroAddress)) {
+        throw new AddressCannotBeZeroError(from);
+      }
+
+      const balance = await this.getBalanceOf(from);
+      if (!amount.isLessThanOrEqualTo(balance)) {
+        throw new OutOfBalanceError(amount.toNumber(), balance.toNumber());
+      }
+    };
+
+    return this.createTransaction({ from, method: 'burn', args, validate });
   }
 }
 
