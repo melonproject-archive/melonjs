d67a7d6726c2daa77bb9cbc50d495d66c78c620b
Sebastian Siemssen <fubhy@fubhy.com>
2020-01-14 16:30:47 +0100

    Add label to function signature.

--- src/utils/decodeFunctionSignature.ts
+++ src/utils/decodeFunctionSignature.ts
@@ -1,29 +1,30 @@
 export interface DecodedFunctionSignature {
-  function: string | undefined;
-  contract: string | undefined;
+  label: string;
+  function: string;
+  contract: string;
 }
 
-export const decodeFunctionSignature = (signature: string): DecodedFunctionSignature => {
+export function decodeFunctionSignature(signature: string): DecodedFunctionSignature | undefined {
   switch (signature) {
     case '0xe51be6e8f1c20394ac4bc9b52300bea4a3697c14c468087e25e8b916b34aa373':
       return {
+        label: 'take',
         function: 'takeOrder',
         contract: 'ExchangeAdapter',
       };
     case '0x79705be7d675563c1e2321f67e8b325f7dd168f51975b104d5f4588cf7e82725':
       return {
+        label: 'make',
         function: 'makeOrder',
         contract: 'ExchangeAdapter',
       };
     case '0x613466791ec33946b8819ce34672fed07c05cbddfd8152db7f548a582612dde9':
       return {
+        label: 'cancel',
         function: 'cancelOrder',
         contract: 'ExchangeAdapter',
       };
-    default:
-      return {
-        function: undefined,
-        contract: undefined,
-      };
   }
-};
+
+  return undefined;
+}
