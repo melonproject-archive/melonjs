37511e12681a0302316a060b11aba84e0ebbfafb
Ivan Herger <ivan.herger@gmail.com>
2020-01-27 16:43:08 +0100

    Add uniswap trading adapter

---
+++ src/contracts/fund/trading/exchanges/UniswapTradingAdapter.ts
@@ -0,0 +1,71 @@
+import BigNumber from 'bignumber.js';
+import { Address } from '../../../../Address';
+import { functionSignature } from '../../../../utils/functionSignature';
+import { ExchangeAdapterAbi } from '../../../../abis/ExchangeAdapter.abi';
+import { zeroAddress } from '../../../../utils/zeroAddress';
+import { checkSufficientBalance } from '../utils/checkSufficientBalance';
+import { checkFundIsNotShutdown } from '../utils/checkFundIsNotShutdown';
+import { zeroBigNumber } from '../../../../utils/zeroBigNumber';
+import { BaseTradingAdapter } from './BaseTradingAdapter';
+import { CallOnExchangeArgs } from '../Trading';
+import { padLeft } from 'web3-utils';
+import { checkSenderIsFundManager } from '../utils/checkSenderIsFundManager';
+
+export interface UniswapTakeOrderArgs {
+  makerAsset: Address;
+  takerAsset: Address;
+  makerQuantity: BigNumber;
+  takerQuantity: BigNumber;
+}
+
+export class UniswapTradingAdapter extends BaseTradingAdapter {
+  /**
+   * Take order on Uniswap
+   *
+   * @param from The address of the sender
+   * @param args The arguments as [[UniswapTakeOrderArgs]]
+   */
+  public takeOrder(from: Address, args: UniswapTakeOrderArgs) {
+    const paddedZeros = padLeft('0x0', 64);
+    const methodArgs: CallOnExchangeArgs = {
+      exchangeIndex: this.index,
+      methodSignature: functionSignature(ExchangeAdapterAbi, 'takeOrder'),
+      orderAddresses: [
+        zeroAddress,
+        zeroAddress,
+        args.makerAsset,
+        args.takerAsset,
+        zeroAddress,
+        zeroAddress,
+        zeroAddress,
+        zeroAddress,
+      ],
+      orderValues: [
+        args.makerQuantity,
+        args.takerQuantity,
+        zeroBigNumber,
+        zeroBigNumber,
+        zeroBigNumber,
+        zeroBigNumber,
+        args.takerQuantity,
+        zeroBigNumber,
+      ],
+      orderData: [paddedZeros, paddedZeros, paddedZeros, paddedZeros],
+      identifier: paddedZeros,
+      signature: paddedZeros,
+    };
+
+    const validate = async () => {
+      const vaultAddress = (await this.trading.getRoutes()).vault;
+      const hubAddress = await this.trading.getHub();
+
+      await Promise.all([
+        checkSufficientBalance(this.trading.environment, args.takerAsset, args.takerQuantity, vaultAddress),
+        checkFundIsNotShutdown(this.trading.environment, hubAddress),
+        checkSenderIsFundManager(this.trading.environment, from, hubAddress),
+      ]);
+    };
+
+    return this.trading.callOnExchange(from, methodArgs, validate);
+  }
+}
