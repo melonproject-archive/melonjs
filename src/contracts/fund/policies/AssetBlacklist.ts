9168cbd00add96fce627b18f23dcf04aaad5bee5
Ivan Herger <ivan.herger@gmail.com>
2020-02-25 12:59:48 +0100

    Add transactions to list policies

--- src/contracts/fund/policies/AssetBlacklist.ts
+++ src/contracts/fund/policies/AssetBlacklist.ts
@@ -1,7 +1,7 @@
 import { Contract } from '../../../Contract';
 import { applyMixins } from '../../../utils/applyMixins';
 import { IPolicy } from './IPolicy';
-import { AddressList } from './AddressList';
+import { AddressList, IsAlreadyMemberError } from './AddressList';
 import { AssetBlacklistAbi } from '../../../abis/AssetBlacklist.abi';
 import { Environment } from '../../../Environment';
 import { Address } from '../../../Address';
@@ -11,6 +11,22 @@ export class AssetBlacklist extends Contract {
 
   public static deploy(environment: Environment, bytecode: string, from: Address, members: Address[]) {
     return super.createDeployment<AssetBlacklist>(environment, bytecode, from, [members]);
+  }
+
+  /**
+   * Adds a list of assets to an AssetBlacklist.
+   *
+   * @param from The address of the sender
+   * @param asset The address of the asset to be added to the AssetBlacklist
+   */
+  public addToBlacklist(from: Address, asset: Address) {
+    const validate = async () => {
+      if (this.isMember(asset)) {
+        throw new IsAlreadyMemberError(asset);
+      }
+    };
+
+    return this.createTransaction({ from, method: 'addToBlacklist', args: [asset], validate });
   }
 }
 
--- src/contracts/fund/policies/UserWhitelist.ts
+++ src/contracts/fund/policies/UserWhitelist.ts
@@ -11,6 +11,46 @@ export class UserWhitelist extends IPolicy {
   }
 
   /**
+   * Add a user address to a UserWhitelist.
+   *
+   * @param from The address of the sender
+   * @param user The address of the user to be added to the UserWhitelist
+   */
+  public addToWhitelist(from: Address, user: Address) {
+    return this.createTransaction({ from, method: 'addToWhitelist', args: [user] });
+  }
+
+  /**
+   * Remove a user address from a UserWhitelist.
+   *
+   * @param from The address of the sender
+   * @param user The address of the user to be removed from the UserWhitelist
+   */
+  public removeFromWhitelist(from: Address, user: Address) {
+    return this.createTransaction({ from, method: 'removeFromWhitelist', args: [user] });
+  }
+
+  /**
+   * Add a list of user addresses to a UserWhitelist.
+   *
+   * @param from The address of the sender
+   * @param users The addresses of the users to be added to the UserWhitelist
+   */
+  public batchAddToWhitelist(from: Address, users: Address[]) {
+    return this.createTransaction({ from, method: 'batchAddToWhitelist', args: [users] });
+  }
+
+  /**
+   * Remove a list of user addresses from a UserWhitelist.
+   *
+   * @param from The address of the sender
+   * @param users The addresses of the users to be removed from the UserWhitelist
+   */
+  public batchRemoveFromWhitelist(from: Address, users: Address[]) {
+    return this.createTransaction({ from, method: 'batchRemoveFromWhitelist', args: [users] });
+  }
+
+  /**
    * Checks if an address is whitelisted
    *
    * @param address The address to check
