a3a65e404fa0cfd8f67885dc98347ac8778795de
Sebastian Siemssen <fubhy@fubhy.com>
2020-04-06 19:18:52 +0200

    Remove ganache environment.

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -4,8 +4,6 @@ module.exports = {
   },
   rootDir: '..',
   roots: ['<rootDir>/src'],
-  globalSetup: '<rootDir>/tests/jest.setup.js',
-  globalTeardown: '<rootDir>/tests/jest.teardown.js',
   setupFilesAfterEnv: ['<rootDir>/tests/jest.timeout.js'],
   moduleFileExtensions: ['js', 'ts'],
   globals: {
--- tests/jest.setup.js
+++
@@ -1,6 +0,0 @@
-const ganache = require('ganache-core');
-
-module.exports = () => {
-  global.__GANACHE__ = ganache.server();
-  global.__GANACHE__.listen(process.env.GANACHE_PORT || 8555);
-};
